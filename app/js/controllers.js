847c39920d3fe3408909db879f258aa15629691c
Joshua Sebastian <jbs87@hotmail.com>
2015-01-05 21:57:23 -0500

    Added table. Implemented remove party

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,11 +6,12 @@ angular.module('myApp.controllers', [])
   .controller('LandingPageController',[function(){
 
   	}])
-  .controller('WaitlistController',['$scope', function($scope){
-  	$scope.parties = [];
-  	$scope.party = {name:'',phone:'',size:''};
+  .controller('WaitlistController',['$scope','$firebase', function($scope,$firebase){
+  	var partiesRef = new Firebase('https://waitandeat-josh.firebaseio.com/');
+  	$scope.parties = $firebase(partiesRef);
+  	$scope.newParty = {name:'',phone:'',size:''};
   	$scope.saveParty = function(){
-  		$scope.parties.push($scope.party);
-  		$scope.party = {name:'',phone:'',size:''};
+  		$scope.parties.$add($scope.newParty);
+  		$scope.newParty = {name:'',phone:'',size:''};
   	};
   }]);
